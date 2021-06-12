635e629ea7c4daacde5dee54dc7e89cb946b2ce9
Dominik <dmkk1@outlook.com>
2021-06-12 06:49:01 +0300

    Upload Exercise 3.15

--- index.js
+++ index.js
@@ -116,10 +116,14 @@ app.get('/info', (req, res) => {
   })
   
   app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+    // const id = Number(request.params.id)
+    // persons = persons.filter(person => person.id !== id)
   
-    response.status(204).end()
+    // response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
   })
   
   const PORT = process.env.PORT
